2ca0948322351aa8feba881a9a6b70baa9f9871c
Agniva De Sarker <agnivade@yahoo.co.in>
2019-05-24 00:31:05 +0200

    Major refactor: Use ChromeDP instead of using chrome driver binary
    - It is a pure Go solution now without any external dependencies.
    - No need to set any environment variables.
    - No dependency on external asset folders. The index.html is hardcoded,
    but wasm_exec.js is picked up from runtime.GOROOT()

--- handler.go
+++ handler.go
@@ -8,17 +8,42 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"runtime"
+	"strconv"
 	"time"
 )
 
 type wasmServer struct {
-	indexHTML   *template.Template
-	wasmFile    string
-	assetFolder string
-	args        []string
+	indexTmpl  *template.Template
+	wasmFile   string
+	wasmExecJS []byte
+	args       []string
+	logger     *log.Logger
+}
+
+func NewWasmServer(wasmFile string, args []string, l *log.Logger) (http.Handler, error) {
+	var err error
+	srv := &wasmServer{
+		wasmFile: wasmFile,
+		args:     args,
+		logger:   l,
+	}
+
+	buf, err := ioutil.ReadFile(path.Join(runtime.GOROOT(), "misc/wasm/wasm_exec.js"))
+	if err != nil {
+		return nil, err
+	}
+	srv.wasmExecJS = buf
+
+	srv.indexTmpl, err = template.New("index").Parse(indexHTML)
+	if err != nil {
+		return nil, err
+	}
+	return srv, nil
 }
 
 func (ws *wasmServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// log.Println(r.URL.Path)
 	switch r.URL.Path {
 	case "/", "/index.html":
 		w.Header().Set("Content-Type", "text/html; charset=UTF-8")
@@ -29,39 +54,76 @@ func (ws *wasmServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			WASMFile: filepath.Base(ws.wasmFile),
 			Args:     ws.args,
 		}
-		err := ws.indexHTML.Execute(w, data)
+		err := ws.indexTmpl.Execute(w, data)
 		if err != nil {
-			log.Println(err)
+			ws.logger.Println(err)
 		}
 	case "/" + filepath.Base(ws.wasmFile):
 		f, err := os.Open(ws.wasmFile)
 		if err != nil {
-			log.Println(err)
+			ws.logger.Println(err)
 			return
 		}
 		defer f.Close()
 		http.ServeContent(w, r, r.URL.Path, time.Now(), f)
-	default:
-		http.ServeFile(w, r, path.Join(ws.assetFolder, r.URL.Path))
+	case "/wasm_exec.js":
+		w.Header().Set("Content-Type", "application/javascript")
+		w.Header().Set("Content-Length", strconv.Itoa(len(ws.wasmExecJS)))
+		if _, err := w.Write(ws.wasmExecJS); err != nil {
+			ws.logger.Println("unable to write wasm_exec.")
+		}
 	}
 }
 
-func getHandler(assetFolder, wasmFile string, args []string) (http.Handler, error) {
-	var err error
-	srv := &wasmServer{
-		wasmFile:    wasmFile,
-		assetFolder: assetFolder,
-		args:        args,
-	}
+const indexHTML = `<!doctype html>
+<!--
+Copyright 2018 The Go Authors. All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+-->
+<html>
 
-	// Read index.html and store as template.
-	indexBuf, err := ioutil.ReadFile(path.Join(assetFolder, "index.html"))
-	if err != nil {
-		return srv, err
-	}
-	srv.indexHTML, err = template.New("index").Parse(string(indexBuf))
-	if err != nil {
-		return srv, err
-	}
-	return srv, nil
-}
+<head>
+	<meta charset="utf-8">
+	<title>Go wasm</title>
+</head>
+
+<body>
+	<!--
+	Add the following polyfill for Microsoft Edge 17/18 support:
+	<script src="https://cdn.jsdelivr.net/npm/text-encoding@0.7.0/lib/encoding.min.js"></script>
+	(see https://caniuse.com/#feat=textencoder)
+	-->
+	<script src="wasm_exec.js"></script>
+	<script>
+		if (!WebAssembly.instantiateStreaming) { // polyfill
+			WebAssembly.instantiateStreaming = async (resp, importObject) => {
+				const source = await (await resp).arrayBuffer();
+				return await WebAssembly.instantiate(source, importObject);
+			};
+		}
+
+		let exitCode = 0;
+		function goExit(code) {
+			exitCode = code;
+		}
+
+		(async() => {
+			const go = new Go();
+			go.argv = [{{range $i, $item := .Args}} {{if $i}}, {{end}} "{{$item}}" {{end}}];
+			go.exit = goExit;
+			let mod, inst;
+			await WebAssembly.instantiateStreaming(fetch("{{.WASMFile}}"), go.importObject).then((result) => {
+				mod = result.module;
+				inst = result.instance;
+			}).catch((err) => {
+				console.error(err);
+			});
+			await go.run(inst);
+			document.getElementById("doneButton").disabled = false;
+		})();
+	</script>
+
+	<button id="doneButton" disabled>Done</button>
+</body>
+</html>`
--- main.go
+++ main.go
@@ -11,113 +11,82 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/sclevine/agouti"
+	cdpruntime "github.com/chromedp/cdproto/runtime"
+	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	chromeDriverPath := os.Getenv("CHROME_DRIVER") // "drivers/chromedriver-linux64-2.35"
-	if chromeDriverPath == "" {
-		log.Fatal("env var CHROME_DRIVER is not set.")
-	}
-
-	assetFolder := os.Getenv("ASSET_FOLDER") // "./assets"
-	if assetFolder == "" {
-		log.Fatal("env var ASSET_FOLDER is not set.")
-	}
-
+	logger := log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile)
 	if len(os.Args) < 2 {
-		log.Fatal("Please pass a wasm file as a parameter")
+		logger.Fatal("Please pass a wasm file as a parameter")
 	}
 	wasmFile := os.Args[1]
 
 	// Need to generate a random port every time for tests in parallel to run.
 	port, err := rand.Int(rand.Reader, big.NewInt(2000))
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 	portStr := ":" + strconv.Itoa(int(port.Int64())+5000)
 
 	// Setup web server.
-	handler, err := getHandler(assetFolder, wasmFile, os.Args[1:])
+	handler, err := NewWasmServer(wasmFile, os.Args[1:], logger)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 	httpServer := &http.Server{
 		Addr:    portStr,
 		Handler: handler,
 	}
 
+	// create chrome instance
+	ctx, cancel := chromedp.NewContext(
+		context.Background(),
+	)
+	defer cancel()
+
+	chromedp.ListenTarget(ctx, func(ev interface{}) {
+		switch ev := ev.(type) {
+		case *cdpruntime.EventConsoleAPICalled:
+			for _, arg := range ev.Args {
+				s, err := strconv.Unquote(string(arg.Value))
+				if err != nil {
+					logger.Println(err)
+					continue
+				}
+				fmt.Printf("%s\n", s)
+			}
+		}
+	})
+
+	done := make(chan struct{})
 	go func() {
 		err := httpServer.ListenAndServe()
 		if err != http.ErrServerClosed {
-			log.Println(err)
+			logger.Println(err)
 		}
+		done <- struct{}{}
 	}()
 
-	opts := []agouti.Option{
-		agouti.ChromeOptions(
-			"args", []string{
-				"headless",
-				"verbose",
-				"unlimited-quota-for-files",
-			},
-		),
-		agouti.Desired(
-			agouti.Capabilities{
-				"loggingPrefs": map[string]string{
-					"browser": "INFO",
-				},
-			},
-		),
-	}
-
-	driver := agouti.NewWebDriver("http://{{.Address}}", []string{chromeDriverPath, "--port={{.Port}}"}, opts...)
-	if err := driver.Start(); err != nil {
-		log.Fatal("Failed to start Selenium:", err)
-	}
-	page, err := driver.NewPage(agouti.Browser("chrome"))
-	if err != nil {
-		log.Fatal("Failed to open page:", err)
-	}
-
-	if err := page.Navigate("http://localhost" + portStr); err != nil {
-		log.Fatal("Failed to navigate:", err)
-	}
-
-	// Wait to finish.
-	goExited := false
-	for !goExited {
-		page.RunScript("return go.exited;", nil, &goExited)
-		if !goExited {
-			time.Sleep(500 * time.Millisecond)
-		}
-	}
-
-	// Capture logs.
-	logs, err := page.ReadAllLogs("browser")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, l := range logs {
-		fmt.Println(l.Message)
-	}
-
 	exitCode := 0
-	page.RunScript("return go.exitCode;", nil, &exitCode)
+	err = chromedp.Run(ctx,
+		chromedp.Navigate(`http://localhost`+portStr),
+		chromedp.WaitEnabled(`#doneButton`),
+		chromedp.Evaluate(`exitCode;`, &exitCode),
+	)
+	if err != nil {
+		logger.Fatal(err)
+	}
 	if exitCode != 0 {
-		// Test has failed. Set the exit code to 1 on the way out after closing all resources.
 		defer os.Exit(1)
 	}
-
-	// Close shop.
-	if err := driver.Stop(); err != nil {
-		log.Fatal(err)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	// create a timeout
+	ctx, cancel = context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	// Close shop
 	err = httpServer.Shutdown(ctx)
 	if err != nil {
-		log.Println(err)
+		logger.Println(err)
 	}
-	cancel()
+	<-done
 }
