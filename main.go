800c80a2c2915392b7b5e54410433122041687bd
Agniva De Sarker <agnivade@yahoo.co.in>
2019-06-28 13:24:26 +0530

    Parse the command line flags to get the cpuprofile arg

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -24,6 +25,7 @@ func main() {
 	if len(os.Args) < 2 {
 		logger.Fatal("Please pass a wasm file as a parameter")
 	}
+
 	wasmFile := os.Args[1]
 	ext := path.Ext(wasmFile)
 	// net/http code does not take js/wasm path if it is a .test binary.
@@ -35,6 +37,13 @@ func main() {
 		}
 		os.Args[1] = wasmFile
 	}
+	// We create a copy of the args to pass to NewWASMServer, because flag.Parse needs the
+	// 2nd argument (the binary name) removed before being called.
+	// This is an effect of "go test" passing all the arguments _after_ the binary name.
+	argsCopy := append([]string(nil), os.Args...)
+	// Remove the 2nd argument.
+	os.Args = append(os.Args[:1], os.Args[2:]...)
+	flag.Parse()
 
 	// Need to generate a random port every time for tests in parallel to run.
 	l, err := net.Listen("tcp", "localhost:")
@@ -51,7 +60,7 @@ func main() {
 	}
 
 	// Setup web server.
-	handler, err := NewWASMServer(wasmFile, os.Args[1:], logger)
+	handler, err := NewWASMServer(wasmFile, filterCPUProfile(argsCopy[1:]), logger)
 	if err != nil {
 		logger.Fatal(err)
 	}
@@ -109,18 +118,23 @@ func main() {
 	}()
 
 	exitCode := 0
-	err = chromedp.Run(ctx,
-		profiler.Enable(),
-		profiler.Start(),
-		chromedp.Navigate(`http://localhost:`+port),
+	tasks := []chromedp.Action{
+		chromedp.Navigate(`http://localhost:` + port),
 		chromedp.WaitEnabled(`#doneButton`),
 		chromedp.Evaluate(`exitCode;`, &exitCode),
-		chromedp.ActionFunc(func(ctx context.Context) error {
+	}
+	if *cpuProfile != "" {
+		// Prepend and append profiling tasks
+		tasks = append([]chromedp.Action{
+			profiler.Enable(),
+			profiler.Start(),
+		}, tasks...)
+		tasks = append(tasks, chromedp.ActionFunc(func(ctx context.Context) error {
 			profile, err := profiler.Stop().Do(ctx)
 			if err != nil {
 				return err
 			}
-			outF, err := os.Create("pprof.out")
+			outF, err := os.Create(*cpuProfile)
 			if err != nil {
 				return err
 			}
@@ -131,8 +145,10 @@ func main() {
 				}
 			}()
 			return WriteProfile(profile, outF)
-		}),
-	)
+		}))
+	}
+
+	err = chromedp.Run(ctx, tasks...)
 	if err != nil {
 		logger.Println(err)
 	}
@@ -148,4 +164,19 @@ func main() {
 		logger.Println(err)
 	}
 	<-done
+}
+
+// filterCPUProfile removes the cpuprofile argument if passed.
+// CPUProfile is taken from the chromedp driver.
+// So it is valid to pass such an argument, but the wasm binary will throw an error
+// since file i/o is not supported inside the browser.
+func filterCPUProfile(args []string) []string {
+	tmp := args[:0]
+	for _, x := range args {
+		if strings.Contains(x, "test.cpuprofile") {
+			continue
+		}
+		tmp = append(tmp, x)
+	}
+	return tmp
 }
